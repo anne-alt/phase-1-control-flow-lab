3a551178e5d7af504bf52fa6817c933b5f834a7b
Anne Ogongo <anneogongo98@gmail.com>
2022-08-16 12:37:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let ride;
+  if (distance <= 400) {
+    ride ='This one is on me!';
+  } 
+   else if (distance <= 2500) {
+    ride = 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500) {
+    ride = 'No can do.';
+  }
+  console.log(ride);
+  return ride;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const message = city === "NYC"?  "Ok, sounds good." : "No go.";
+
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(message){
   // Write your code here!
+  let tip;
+
+  switch (message) {
+
+    case 'generous' :
+      tip = "Thank you so much.";
+      break;
+      
+    case "not as generous" :
+      tip = "Thank you.";
+
+     break;
+
+    default:
+      tip = "Bye.";
+      break;
+  
+  }
+  return tip;
 }
